c2be990b5738657d9819d1d70ab08d6a6e696dac
Ricky Alves <hike.lika@gmail.com>
2023-06-02 01:53:19 -0400

    add Material-UI

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,41 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Ricky Alves</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1>
-          Hello, I'm Ricky Alves
-        </h1>
-
-        {/* Social media links can be added here */}
-        <div className={styles.links}>
-          <a href="https://twitter.com/henriquesuper">Twitter</a>
-          <a href="https://linkedin.com/in/henriquesuper">LinkedIn</a>
-          <a href="https://github.com/henriquesuper">GitHub</a>
-        </div>
-
-      </main>
-
-      <footer className={styles.footer}>
-        © Ricky Alves, 2023
-      </footer>
-    </div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Ricky Alves
+          </Typography>
+          <IconButton color="inherit" href="https://github.com/henriquesuper" target="_blank">
+            <GitHubIcon />
+          </IconButton>
+          <IconButton color="inherit" href="https://linkedin.com/in/rickyalves" target="_blank">
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton color="inherit" href="https://twitter.com/henriquesuper" target="_blank">
+            <TwitterIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Hello, I'm Ricky Alves
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
