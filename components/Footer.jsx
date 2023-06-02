8eae03c8ad9da503770a00fc654cb38ae714526b
Ricky Alves <hike.lika@gmail.com>
2023-06-02 07:16:31 -0400

    edit text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <hr/>
         <div className="footer-container">
           <p>
-            © {new Date().getFullYear()} Ricky's Portfolio
+            © {new Date().getFullYear()} Ricky's Code Chronicles
           </p>
           <div className="social_icons">
             <a
