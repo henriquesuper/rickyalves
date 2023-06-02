87fc1d75fd4006121fc564d62355b5c03d61480a
Ricky Alves <hike.lika@gmail.com>
2023-06-02 16:53:03 -0400

    spacing fix

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import About from '../components/About';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
-
 import Head from 'next/head';
 
 const Home = () => {
