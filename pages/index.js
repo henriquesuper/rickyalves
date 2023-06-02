a5e63a4e8e76213a901f95024fd1bc1c389b11d2
Ricky Alves <hike.lika@gmail.com>
2023-06-02 01:35:43 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-function Home() {
-    return <div>Home 5</div>
-}
+import styles from '../styles/Home.module.css'
 
-export default Home
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Ricky Alves</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Hello, I'm Ricky Alves
+        </h1>
+
+        {/* Social media links */}
+        <div>
+          <a href="https://twitter.com/rickyalves">Twitter</a>
+          <a href="https://linkedin.com/in/rickyalves">LinkedIn</a>
+          <a href="https://github.com/rickyalves">GitHub</a>
+        </div>
+
+      </main>
+
+      <footer>
+        © Ricky Alves, 2023
+      </footer>
+    </div>
+  )
+}
