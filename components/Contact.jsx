3da16f138f4aa26305228af47142da92365a5f13
Ricky Alves <hike.lika@gmail.com>
2023-07-06 00:53:32 -0400

    fix email

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
       <div className="contact-container">
         <h2>Summon Me via Cyber Scrolls</h2>
         <p>If you're interested in conjuring up some digital magic together, if your mind is teeming with questions, or if you want me to light up your event with some tech-savvy insights, my inbox is just a keystroke away. Even if you just want to pop in for a virtual 'hello', I'll do my coding-ninja best to respond post-haste! So, here's to breaking the ice with a 'ping', instead of a pint. Over and out!</p>
-        <a href="mailto:hike.lika@gmail.com" className='cta-btn'>Press to Summon Code Wizard</a>
+        <a href="mailto:ricky@rickyalves.com" className='cta-btn'>Press to Summon Code Wizard</a>
       </div>
     )
   }
