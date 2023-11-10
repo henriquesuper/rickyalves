c5e0a0f564ee5d1aad7639fe518091d72e67f4f8
Ricky Alves <hike.lika@gmail.com>
2023-11-10 08:50:05 -0500

    upgraded fontAwesome

--- pages/_document.js
+++ pages/_document.js
@@ -5,13 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
+        {/* Add the Font Awesome Script here */}
+        <script src="https://kit.fontawesome.com/b4b5469beb.js" crossorigin="anonymous"></script>
       </Head>
       <body>
         <Main />
