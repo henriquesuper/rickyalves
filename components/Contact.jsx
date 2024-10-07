681b7b6e7f2613de1d2d4a721d01833ae6a29c2c
Ricky Alves <hike.lika@gmail.com>
2024-10-07 02:12:54 -0400

    fixed email address

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
       <div className="flex flex-col items-center py-8 px-8 w-4/5 md:w-full mx-auto">
         <h2 className="text-3xl font-semibold mb-4">Summon Me via Cyber Scrolls</h2>
         <p className="text-base mb-4 text-center">If you're interested in conjuring up some digital magic together, if your mind is teeming with questions, or if you want me to light up your event with some tech-savvy insights, my inbox is just a keystroke away. Even if you just want to pop in for a virtual 'hello', I'll do my coding-ninja best to respond post-haste! So, here's to breaking the ice with a 'ping', instead of a pint. Over and out!</p>
-        <a href="mailto:ricky@rickyalves.com" className="bg-blue-500 text-white px-4 py-2 rounded-md text-base font-semibold transition-all duration-500 ease-in-out hover:bg-blue-600 cursor-pointer">Press to Summon Code Wizard</a>
+        <a href="mailto:hello@rickyalves.com" className="bg-blue-500 text-white px-4 py-2 rounded-md text-base font-semibold transition-all duration-500 ease-in-out hover:bg-blue-600 cursor-pointer">Press to Summon Code Wizard</a>
       </div>
     )
   }
